dab4ca3c040d57033d6e5510b779e2b0d92585a3
carey <114056147+carey-hu@users.noreply.github.com>
2026-01-24 19:58:49 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    }
+  },
+  build: {
+    // 优化构建
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      }
+    }
+  }
 })
